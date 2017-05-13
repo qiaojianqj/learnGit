94496a6f492e704365312f4700db68de7786ddfe
qiaojian <qiaojianwm@gmail.com>
2017-05-13 12:59:06 +0800

    deleted:    learnC++/a.out 	new file:   learnC++/calc.c 	new file:   learnC++/calc.o 	new file:   learnC++/libcalc.a 	new file:   learnC++/libcalc.djfld 	new file:   learnC++/libcalc.so 	new file:   learnC++/readme 	new file:   learnC++/test 	new file:   learnC++/test.c

---
+++ learnC++/calc.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int add(int a, int b)
+{
+	printf("in add \n");
+	return a+b;
+}
+
+
+int sub(int a, int b)
+{
+	printf("int sub\n");
+	return a - b;
+}
---
+++ learnC++/test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+	int a = 8, b = 5;
+	printf("a - b = %d\n", sub(a, b));
+	printf("a + b = %d\n", add(a, b));
+	return 0;
+}
