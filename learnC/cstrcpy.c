c44d2043604f43d7d116339be92433e9f2a93865
Qiao Jian <qiaojian@JiandeMBP.lan>
2017-05-24 00:11:49 +0800

    new file:   learnC/cstrcpy.c

---
+++ learnC/cstrcpy.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *mystrcpy(char *dest, char *src)
+{
+	char * head = dest;
+	while((*dest++ = *src++) != '\0');
+	return head;
+}
+
+char *mystrncpy(char *dest, char *src, size_t count)
+{
+	char *head = dest;
+	while(count-- && (*dest++ = *src++) != '\0');
+	return head;
+}
+
+char * mystrcat(char * dest, const char * src)
+{
+	char *head = dest;
+	while (*dest)
+		dest++;
+	while ((*dest++ = *src++) != '\0');
+	return head;
+}
+
+int main()
+{
+	char *str1 = "what fuck";
+	char *str2 = (char *)malloc(sizeof(char)*20); 
+	char *str3 = (char *)malloc(sizeof(char)*20); 
+
+	char *str4 = mystrcpy(str2, str1);	
+	printf("str1: %s, str2: %s\n", str1, str2);
+	printf("str4: %s\n", str4);
+	
+    char *str5 = mystrncpy(str3, str1, 4);	
+	printf("str1: %s, str3: %s\n", str1, str3);
+	printf("str5: %s\n", str5);
+
+	char *str6 = mystrcat(str5, str2);
+	printf("str6: %s\n", str6);
+
+	free(str2);
+	free(str3);
+	return 0;
+}
